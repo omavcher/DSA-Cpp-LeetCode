5ca63d54bba2e8f98e9f02c528f56547ba902e7a
Om Avcher <omawchar07@gmial.com>
2025-06-25 06:50:40 +0530

    Add Problem

---
+++ String/GenerateTagforVideoCaption.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string generateTag(string caption) {
+        vector<string> words;
+        string word = "";
+        
+        for (char c : caption) {
+            if (c == ' ') {
+                if (!word.empty()) {
+                    words.push_back(word);
+                    word = "";
+                }
+            } else {
+                word += c;
+            }
+        }
+        if (!word.empty()) words.push_back(word);
+        
+        string tag = "#";
+        for (int i = 0; i < words.size(); i++) {
+            string current = words[i];
+            for (char& ch : current) ch = tolower(ch);
+            if (i > 0 && !current.empty()) {
+                current[0] = toupper(current[0]);
+            }
+            tag += current;
+        }
+
+        if (tag.size() > 100) {
+            tag = tag.substr(0, 100);
+        }
+
+        return tag;
+    }
+};
